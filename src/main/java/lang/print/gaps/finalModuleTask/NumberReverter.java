209277a8988ce376f43852b91c7358933c957934
AndriiVasyliuk <AndriiVasyliukPro@gmail.com>
2023-12-02 22:06:23 +0200

    [modify] do all my exam tasks

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print(number % 10);
+            number /= 10;
+        }
+        System.out.println();
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.finalModuleTask;
 
+// °F = (°C ×9/5) + 32
+// °C = (°F − 32) x 5/9
+
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        System.out.println((double) temperatureCelsius * 9 / 5 + 32);
     }
 }
